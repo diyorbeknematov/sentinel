ea809995a4522a804b1b48e656551d909ae97bfc
Diyorbek Ne'matov <diyorbeknematov0321@gmail.com>
2026-04-04 02:20:02 +0500

    was done system design and created database tables

---
+++ server/internal/migrations/000001_monitoring_db.down.sql
@@ -0,0 +1,11 @@
+-- alerts jadvali
+DROP TABLE IF EXISTS alerts;
+
+-- logs jadvali
+DROP TABLE IF EXISTS logs;
+
+-- metrics jadvali
+DROP TABLE IF EXISTS metrics;
+
+-- agents jadvali
+DROP TABLE IF EXISTS agents;
---
+++ server/internal/migrations/000001_monitoring_db.up.sql
@@ -0,0 +1,55 @@
+-- agents jadvali
+CREATE TABLE IF NOT EXISTS agents (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    api_key VARCHAR(255) UNIQUE NOT NULL,
+    ip_address INET,
+    last_seen TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- metrics jadvali
+CREATE TABLE IF NOT EXISTS metrics (
+    id SERIAL PRIMARY KEY,
+    agent_id INT NOT NULL,
+    cpu NUMERIC(5,2),
+    ram NUMERIC(5,2),
+    disk NUMERIC(5,2),
+    recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_metrics_agent
+        FOREIGN KEY (agent_id)
+        REFERENCES agents(id)
+        ON DELETE CASCADE
+);
+
+-- logs jadvali
+CREATE TABLE IF NOT EXISTS logs (
+    id SERIAL PRIMARY KEY,
+    agent_id INT NOT NULL,
+    type VARCHAR(50),
+    level VARCHAR(20),
+    message TEXT,
+    ip_address INET,
+    recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_logs_agent
+        FOREIGN KEY (agent_id)
+        REFERENCES agents(id)
+        ON DELETE CASCADE
+);
+
+-- alerts jadvali
+CREATE TABLE IF NOT EXISTS alerts (
+    id SERIAL PRIMARY KEY,
+    agent_id INT NOT NULL,
+    type VARCHAR(50),
+    message TEXT,
+    severity VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_alerts_agent
+        FOREIGN KEY (agent_id)
+        REFERENCES agents(id)
+        ON DELETE CASCADE
+);
